e6927317717e48d5aa7e42da727e46932669060c
Arseniy Graur <argraur12@yandex.ru>
2016-11-06 18:28:22 +0300

    Update pixelrom.mk

--- product/pixelrom.mk
+++ product/pixelrom.mk
@@ -1,24 +1,34 @@
+# Set overlay
 PRODUCT_PACKAGE_OVERLAYS := \
 	vendor/pixelrom/product/overlay-pixel \
 
+# Load PixelSoundResources
 PRODUCT_PACKAGES := \
 	PixelSoundResources 
 
+# Set Pixel sounds as Default
 PRODUCT_PROPERTY_OVERRIDES := \
 	ro.config.ringtone=Zen.ogg \
 	ro.config.notification_sound=Chime.ogg \
 	ro.config.alarm_alert=Flow.ogg \
 
+# Copy bootanimation from Pixel
 PRODUCT_COPY_FILES := vendor/pixelrom/data/media/bootanimation.zip:system/media/bootanimation.zip:pixelrom \
-	vendor/pixelrom/data/data-pixel/nexus.xml:system/etc/sysconfig/nexus.xml:pixelrom
 
+# Apply some build.prop stuff and enable Google Assistant
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.setupwizard.rotation_locked=true \
 	ro.opa.eligible_device=true \
 	ro.storage_manager.enabled=true \
 
+# ADB Secure enable on user builds
 ifeq ($(TARGET_BUILD_VARIANT),user)
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES := \
 	ro.adb.secure=1 \
 
 endif
+
+# Load SystemUIGoogle and SettingsGoogle
+PRODUCT_PACKAGES += \
+    SystemUIGoogle
+    SettingsGoogle
