da0d3c37d884beb5d335ad6738e248d37bf13077
Arseniy Graur <argraur12@yandex.ru>
2016-12-06 07:45:15 +0200

    PixelROM: 7.1.1 Updates

--- pixelrom.mk
+++ pixelrom.mk
@@ -5,7 +5,7 @@ PRODUCT_PROPERTY_OVERRIDES := \
 	ro.config.alarm_alert=Flow.ogg \
 
 # Copy bootanimation from Pixel
-PRODUCT_COPY_FILES := vendor/pixelrom/media/bootanimation.zip:system/media/bootanimation.zip:lge \
+PRODUCT_COPY_FILES := vendor/pixelrom/media/bootanimation.zip:system/media/bootanimation.zip:google \
 
 # Apply some build.prop stuff and enable Google Assistant
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -14,10 +14,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.storage_manager.enabled=true \
 
 # ADB Secure enable on user builds
-ifeq ($(TARGET_BUILD_VARIANT),user)
+ifeq ($(TARGET_BUILD_VARIANT),userdebug)
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES := \
-	ro.adb.secure=1 \
-
+	ro.adb.secure=0 \
 endif
 
 # Load SystemUIGoogle and friends :D
@@ -30,18 +29,3 @@ PRODUCT_PACKAGES += \
 	WallpapersUsTwo \
 	WallpaperPickerGooglePrebuilt \
 	NexusWallpapersStubPrebuilt
-
-# Copy GES libraries
-GES_LIBS := vendor/pixelrom/libs
-GES_LIBS86 := $(GES_LIBS)/arm-v7
-GES_LIBS64 := $(GES_LIBS)/arm64-v8a
-PRODUCT_COPY_FILES := \
-	$(GES_LIBS86)/libjpeg.so:system/lib/libjpeg.so:google \
-	$(GES_LIBS64)/libgdx.so:system/lib64/libgdx.so:google \
-	$(GES_LIBS64)/libgeswallpapers-jni.so:system/lib64/libgeswallpapers-jni:google \
-	$(GES_LIBS64)/libjpeg.so:system/lib64/libjpeg.so:google
-
-# Copy new mixer-paths
-MIXER_PATHS_XML := vendor/pixelrom/patches/mixer_paths.xml
-PRODUCT_COPY_FILES := \
-	$(MIXER_PATHS_XML):system/etc/mixer_paths.xml:google
